c172332c31c83dd6ac1df3bbc553e6f93461ef80
Danimx88 <105736149+Danimx88@users.noreply.github.com>
2022-07-18 05:40:09 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+// variable - let const
+
+
+console.log('Iniciando prueba')
+let cliente = "Juan"
+let precioDescuento = 220
+
+console.log(cliente)
+
+// Re asignar
+cliente = 20;
+cliente = true;
+
+// Pueden iniciar sin un valor
+
+let precio
+precio = 1000
+
+console.log(precio)
+console.log(precioDescuento)
